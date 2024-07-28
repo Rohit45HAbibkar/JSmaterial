a09dd89046df5b28fae30534d9c803f909477fc0
Rohit45HAbibkar <rohithabibkar@gmail.com>
2024-07-28 12:07:43 +0530

    commit

---
+++ operations.js
@@ -0,0 +1,37 @@
+
+
+console.log(2**2);
+
+let str ="hello"
+let str2=" hitesh"
+
+let str3=str+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);//122
+// it refers to fist thin to decide what to do
+
+console.log((3*4)*(5%3));
+
+
+console.log(+true);
+console.log(+"");
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+
+let a = 3;
+const b = ++x;
+// x is 4; y is 4
+
+let a1= 3n;
+const b2 = ++x2;
+// x2 is 4n; y2 is 4n
