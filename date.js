911b2d78502123027a40a92917ab63bfb4259ef0
Rohit45HAbibkar <rohithabibkar@gmail.com>
2024-07-28 14:21:37 +0530

    add

---
+++ date.js
@@ -0,0 +1,20 @@
+let mydate=new Date();
+console.log(mydate.toDateString());
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof myDate);
+
+
+//months start form 0 in js
+
+let mytime=Date.now();
+console.log(mytime);
+
+
+let date=new Date();
+
+console.log(Math.floor(date.getTime()));
+
