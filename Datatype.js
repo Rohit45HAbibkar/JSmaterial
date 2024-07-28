bc964386335fdc1a4a3bbcc33daa381a4d54f467
Rohit45HAbibkar <rohithabibkar@gmail.com>
2024-07-28 11:39:01 +0530

    data added

--- Datatype.js
+++ Datatype.js
@@ -1,2 +1,19 @@
 accountcit="bengluru";
 console.log(accountcit);
+
+let name ="rohit"
+let age = 25
+let city = "bangalore"
+
+//number=>2 to power 53
+//bigint 
+//String=>""
+//boolean=>true/false
+//null
+//undefined
+//symbol =>unique
+
+//objects
+
+
+console.log(typeof age);
