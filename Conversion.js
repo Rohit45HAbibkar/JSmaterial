289d840fa088baa8983d01b5658a9fe3b7c29862
Rohit45HAbibkar <rohithabibkar@gmail.com>
2024-07-28 11:51:06 +0530

    conversions

---
+++ Conversion.js
@@ -0,0 +1,16 @@
+let num="123abd"
+console.log(Number(num));
+
+// "33"=>33
+//"33abc"=>NaN (not a number)
+//"true"=>1
+//"fasle"=>0
+
+
+let isLoggedin=1;
+console.log(Boolean(isLoggedin));
+
+//"empty string"=>false;
+//"rohit"=>true;
+
+let somenumberk
