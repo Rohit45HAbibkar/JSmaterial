613ac5d324b72bd1c67425f41a933d65ea74a102
Rohit45HAbibkar <rohithabibkar@gmail.com>
2024-07-28 12:54:03 +0530

    string

---
+++ string.js
@@ -0,0 +1,17 @@
+
+const na="ro-hit"
+
+console.log(`yes :${na}`);
+console.log(na.length);console.log(na.toUpperCase());
+console.log(na.toLowerCase())
+
+console.log(na.charAt(2));
+console.log(na.indexOf('r'));
+
+console.log(na.slice(0,3));
+console.log(na.includes('hit'));
+console.log(na.split('-'));
+console.log(na.bold());
+
+
+
