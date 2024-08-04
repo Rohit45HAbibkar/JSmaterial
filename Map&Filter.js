ba2f3d394c894dd26984f6e288aabfe5bf74165c
Rohit45HAbibkar <rohithabibkar@gmail.com>
2024-08-04 16:33:59 +0530

    map

---
+++ Map&Filter.js
@@ -0,0 +1,24 @@
+
+
+let arr=[1,2,3,4,5,6,7,8,9,10];
+
+arr=arr.filter((num)=>num>4);
+console.log(arr);
+
+//maps
+
+let  maps=[1,2,3,4,5,6,7,8,9,10];
+
+ maps=maps.map((item)=>item+10);
+console.log(maps);
+
+let ans=[1,2,4,5,6];
+
+let b=ans.reduce((num1,num2)=>{return num1+num2},0);
+
+    console.log(b);
+    
+
+
+
+
